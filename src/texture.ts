c20cd2044a8c4bba4fe4bfac25d9db073d80cadd
jrdnrs <55672049+jrdnrs@users.noreply.github.com>
2022-09-13 13:04:17 +0100

    add ability to transpose textures...
    for sake of cache efficiency when reading back during column drawing

--- src/texture.ts
+++ src/texture.ts
@@ -16,7 +16,7 @@ export default class Texture {
         this.height = height;
     }
 
-    static async loadTexture(src: string): Promise<Texture> {
+    static async loadTexture(src: string, transpose = false): Promise<Texture> {
         const r = await fetch(src);
         if (!r.ok) {
             throw r.statusText;
@@ -24,16 +24,31 @@ export default class Texture {
         const blob = await r.blob();
         const img = await createImageBitmap(blob);
         const tmpCanvas = document.createElement("canvas");
-        const tmpCtx = tmpCanvas.getContext("2d")!;
+        const tmpCtx = tmpCanvas.getContext("2d", { alpha: false })!;
         tmpCtx.drawImage(img, 0, 0);
         const bytes = tmpCtx.getImageData(0, 0, img.width, img.height).data;
+        const texture = new Texture(src, img.width, img.height, bytes);
+        if (transpose) texture.transpose();
+        return texture;
+    }
 
-        return new Texture(src, img.width, img.height, bytes);
+    transpose(): this {
+        let transposed = new Uint8ClampedArray(this.bytes.length);
+
+        for (let x = 0; x < this.width; x++) {
+            for (let y = 0; y < this.height; y++) {
+                const readOffset = (y * this.width + (this.width - x)) * 4 - 4;
+                const writeOffset = (x * this.height + y) * 4;
+                transposed[writeOffset] = this.bytes[readOffset];
+                transposed[writeOffset + 1] = this.bytes[readOffset + 1];
+                transposed[writeOffset + 2] = this.bytes[readOffset + 2];
+                transposed[writeOffset + 3] = this.bytes[readOffset + 3];
+            }
+        }
+
+        this.bytes = transposed;
+        [this.width, this.height] = [this.height, this.width];
+
+        return this;
     }
 }
-
-// return new Promise((r) => {
-//     let i = new Image();
-//     i.onload = () => r(i);
-//     i.src = url;
-// });
